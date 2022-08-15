82f4122fc7d25693f1a0a5fb50e936500bc92ae3
Wissididom <30803034+Wissididom@users.noreply.github.com>
2022-08-15 10:03:51 +0200

    Create Special Discord Ringtone.js
    Replaces the usual ringtone with a ringtone I heard you'd only get in a change of 0.1

---
+++ Special Discord Ringtone.js
@@ -0,0 +1,5 @@
+Audio.prototype._play = Audio.prototype.play;
+Audio.prototype.play = function() {
+    this.src = this.src.replace(/84a1b4e11d634dbfa1e5dd97a96de3ad/g, 'b9411af07f154a6fef543e7e442e4da9');
+    return this._play();
+};
