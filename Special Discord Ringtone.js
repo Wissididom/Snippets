76448bb87506fafe29f6974d4f097422c103ea51
Wissididom <30803034+Wissididom@users.noreply.github.com>
2022-08-15 10:05:25 +0200

    Added Links to the source files

--- Special Discord Ringtone.js
+++ Special Discord Ringtone.js
@@ -3,3 +3,5 @@ Audio.prototype.play = function() {
     this.src = this.src.replace(/84a1b4e11d634dbfa1e5dd97a96de3ad/g, 'b9411af07f154a6fef543e7e442e4da9');
     return this._play();
 };
+// Normal ringtone: https://discord.com/assets/84a1b4e11d634dbfa1e5dd97a96de3ad.mp3
+// Special ringtone: https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3
